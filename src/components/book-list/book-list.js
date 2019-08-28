7836155e1df154f89ad1a13b671419c1283fb2e2
Temirlan <bamblebee988@gmail.com>
2019-08-28 20:07:49 +0600

    l-125: getting data from Redux store

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import BookListItem from "../book-list-item";
-
+import { connect } from "react-redux";
 import "./book-list.css";
 
 class BookList extends Component {
@@ -20,4 +20,8 @@ class BookList extends Component {
   }
 }
 
-export default BookList;
+const mapStateToProps = ({ books }) => {
+  return { books };
+};
+
+export default connect(mapStateToProps)(BookList);
--- src/components/pages/home-page.js
+++ src/components/pages/home-page.js
@@ -5,7 +5,7 @@ const HomePage = () => {
   return (
     <div>
       <h1>HomePage</h1>
-      <BookList books={[]} />
+      <BookList />
     </div>
   );
 };
