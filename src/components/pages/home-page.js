193edad0b5899713fbe04df16125db7509e3494d
Temirlan <bamblebee988@gmail.com>
2019-08-29 08:53:01 +0600

    l-127: add styles

--- src/components/pages/home-page.js
+++ src/components/pages/home-page.js
@@ -1,11 +1,12 @@
 import React from "react";
 import BookList from "../book-list";
+import ShoppingCartTable from "../shopping-cart-table";
 
 const HomePage = () => {
   return (
     <div>
-      <h1>HomePage</h1>
       <BookList />
+      <ShoppingCartTable />
     </div>
   );
 };
