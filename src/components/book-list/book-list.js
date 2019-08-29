dca04457c3acf1062793289a31fe10122d10fdae
Temirlan <bamblebee988@gmail.com>
2019-08-29 09:21:00 +0600

    l-129: setState vs reducer

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -3,14 +3,15 @@ import BookListItem from "../book-list-item";
 import { connect } from "react-redux";
 
 import { withBookstoreService } from "../hoc";
-import { booksLoaded } from "../../actions";
+import { booksRequested, booksLoaded } from "../../actions";
 import { compose } from "../../utils";
 import Spinner from "../spinner";
 import "./book-list.css";
 
 class BookList extends Component {
   componentDidMount() {
-    const { bookstoreService, booksLoaded } = this.props;
+    const { bookstoreService, booksLoaded, booksRequested } = this.props;
+    booksRequested();
     bookstoreService.getBooks().then(data => booksLoaded(data));
   }
 
@@ -40,7 +41,8 @@ const mapStateToProps = ({ books, loading }) => {
 };
 
 const mapDispatchToProps = {
-  booksLoaded
+  booksLoaded,
+  booksRequested
 };
 
 export default compose(
