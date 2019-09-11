81cf8a40c07de3bc0f4ebf212cc17822ce4ae21a
Temirlan <bamblebee988@gmail.com>
2019-09-11 22:11:48 +0600

    l-139: store upgrade dispatch - monkey patching

--- src/store.js
+++ src/store.js
@@ -4,4 +4,20 @@ import reducer from "./reducers";
 
 const store = createStore(reducer);
 
+// monkey patching (bad idea)
+const originalDispatch = store.dispatch;
+store.dispatch = (action) => {
+
+  if (typeof action === "string") {
+    return originalDispatch({
+      type: action
+    });
+  }
+
+  return originalDispatch(action);
+}
+
+
+store.dispatch('hello_world');
+
 export default store;
