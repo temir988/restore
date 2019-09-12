af258bf52e0ca857c12f07e8731a6be238a6ee57
Temirlan <bamblebee988@gmail.com>
2019-09-12 09:15:23 +0600

    l-140: upgrade store - Middleware

--- src/store.js
+++ src/store.js
@@ -1,36 +1,24 @@
-import { createStore, compose } from "redux";
+import { createStore, applyMiddleware } from "redux";
 
 import reducer from "./reducers";
-import { create } from "domain";
 
-const stringEnhancer = (createStore) => (...args) => {
-  const store = createStore(...args);
-  const originalDispatch = store.dispatch;
-  store.dispatch = (action) => {
-  
-    if (typeof action === "string") {
-      return originalDispatch({
-        type: action
-      });
-    }
-  
-    return originalDispatch(action);
-  }
-  return store;
+const logMiddleware = (store) => (next) => (action) => {
+  console.log(action.type, store.getState());
+  return next(action);
 };
 
-const logEnhancer = (createStore) => (...args) => {
-  const store = createStore(...args);
-  const originalDispatch = store.dispatch;
-  store.dispatch = (action) => {
-    console.log(action.type);
-    return originalDispatch(action);
-  };
-  return store;
-}
+const stringMiddleware = () => (next) => (action) => {
+  if (typeof action === "string") {
+    return next({
+      type: action
+    });
+  }
 
-const store = createStore(reducer, compose(stringEnhancer, logEnhancer));
+  return next(action);
+};
 
+const store = createStore(reducer, applyMiddleware(stringMiddleware, logMiddleware));
+  
 store.dispatch("HELLO_WORLD")
 
 
